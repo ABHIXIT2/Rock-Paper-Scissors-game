1337937899be57f8041302e6249db6c6dcc506f7
Abhinav <dabhinav378@gmail.com>
2022-09-29 02:02:13 +0530

    Edit game.js

--- game.js
+++ game.js
@@ -1,9 +1,32 @@
 function getComputerChoice() {
-    
+
     const randomIndex = Math.floor(Math.random() * 3);
-    const array = ["Rock","Paper","Scissor"];
+    const array = ["rock", "paper", "scissor"];
     const item = array[randomIndex];
     return item;
 }
 
 console.log(getComputerChoice());
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection=="rock" && computerSelection =="scissor" ||
+    playerSelection=="scissor" && computerSelection =="paper" ||
+    playerSelection=="paper" && computerSelection =="rock") {
+        const result="You Win";
+        return result;
+    }
+    else if (playerSelection=="scissor" && computerSelection =="rock" ||
+    playerSelection=="paper" && computerSelection =="scissor" ||
+    playerSelection=="rock" && computerSelection =="paper") {
+        const result="You lose";
+        return result;
+    }
+    else{
+        const result="Draw";
+        return result;
+    }
+}
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
